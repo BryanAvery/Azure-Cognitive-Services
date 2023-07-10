b5af7479979fc644233de34bbb91456c3ef9ff3e
Bryan AVERY <bryan.avery@chanel.com>
2023-07-10 14:26:44 +0100

    Removed the main

--- SpeechRecognition/Program.cs
+++ SpeechRecognition/Program.cs
@@ -1,40 +1,34 @@
 ﻿using Microsoft.CognitiveServices.Speech;
 
-class Program
+// Retrieve the subscription key and region from environment variables
+string subscriptionKey = Environment.GetEnvironmentVariable("COGNITIVE_SERVICES_SUBSCRIPTION_KEY");
+string region = Environment.GetEnvironmentVariable("COGNITIVE_SERVICES_REGION");
+
+
+// Configure speech recognition
+var config = SpeechConfig.FromSubscription(subscriptionKey, region);
+config.SpeechRecognitionLanguage = "en-US";
+
+// Create a speech recognizer
+using (var recognizer = new SpeechRecognizer(config))
 {
-    static async Task Main()
+    Console.WriteLine("Say something...");
+
+    // Start recognition
+    var result = await recognizer.RecognizeOnceAsync();
+
+    // Check recognition result
+    if (result.Reason == ResultReason.RecognizedSpeech)
     {
-        // Retrieve the subscription key and region from environment variables
-        string subscriptionKey = Environment.GetEnvironmentVariable("COGNITIVE_SERVICES_SUBSCRIPTION_KEY");
-        string region = Environment.GetEnvironmentVariable("COGNITIVE_SERVICES_REGION");
-
-
-        // Configure speech recognition
-        var config = SpeechConfig.FromSubscription(subscriptionKey, region);
-        config.SpeechRecognitionLanguage = "en-US";
-
-        // Create a speech recognizer
-        using (var recognizer = new SpeechRecognizer(config))
-        {
-            Console.WriteLine("Say something...");
-
-            // Start recognition
-            var result = await recognizer.RecognizeOnceAsync();
-
-            // Check recognition result
-            if (result.Reason == ResultReason.RecognizedSpeech)
-            {
-                Console.WriteLine($"Recognized text: {result.Text}");
-            }
-            else if (result.Reason == ResultReason.NoMatch)
-            {
-                Console.WriteLine("Speech could not be recognized.");
-            }
-            else if (result.Reason == ResultReason.Canceled)
-            {
-                var cancellation = CancellationDetails.FromResult(result);
-                Console.WriteLine($"Speech recognition canceled: {cancellation.Reason}");
-            }
-        }
+        Console.WriteLine($"Recognized text: {result.Text}");
+    }
+    else if (result.Reason == ResultReason.NoMatch)
+    {
+        Console.WriteLine("Speech could not be recognized.");
+    }
+    else if (result.Reason == ResultReason.Canceled)
+    {
+        var cancellation = CancellationDetails.FromResult(result);
+        Console.WriteLine($"Speech recognition canceled: {cancellation.Reason}");
     }
 }
