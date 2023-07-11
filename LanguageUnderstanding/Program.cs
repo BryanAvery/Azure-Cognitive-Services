839a84719c11f43ebece5786df2ba530bed332c6
Bryan AVERY <bryan.avery@chanel.com>
2023-07-11 14:35:19 +0100

    Added Lanugage Understanding Added Open AI Service (Chat GPT 3.5)

---
+++ LanguageUnderstanding/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Azure.CognitiveServices.Language.LUIS.Runtime;
+using Microsoft.Azure.CognitiveServices.Language.LUIS.Runtime.Models;
+
+// Retrieve the subscription key and region from environment variables
+string subscriptionKey = Environment.GetEnvironmentVariable("COGNITIVE_SERVICES_SUBSCRIPTION_KEY");
+string appId = Environment.GetEnvironmentVariable("APPID");
+string endpoint = Environment.GetEnvironmentVariable("ENDPOINT");
+
+// Create a LUIS runtime client
+var credentials = new ApiKeyServiceClientCredentials(subscriptionKey);
+var client = new LUISRuntimeClient(credentials)
+{
+    Endpoint = endpoint
+};
+
+// Replace with your query
+var query = "What is the weather like today?";
+
+// Predict the intent and entities
+var predictionRequest = new PredictionRequest
+{
+    Query = query
+};
+
+var predictionResponse = await client.Prediction.GetSlotPredictionAsync(appId, "production", predictionRequest);
+
+// Process the prediction results
+var topIntent = predictionResponse.Prediction.TopIntent;
+var entities = predictionResponse.Prediction.Entities;
+
+Console.WriteLine($"Top intent: {topIntent}");
+Console.WriteLine("Entities:");
+
+foreach (var entity in entities)
+{
+    Console.WriteLine($"- {entity.Key}: {entity.Value.First}");
+}
---
+++ OpenAIService/Program.cs
@@ -0,0 +1,19 @@
+﻿using Azure;
+using Azure.AI.OpenAI;
+
+// Retrieve the subscription key and region from environment variables
+string subscriptionKey = Environment.GetEnvironmentVariable("COGNITIVE_SERVICES_SUBSCRIPTION_KEY");
+string endpoint = Environment.GetEnvironmentVariable("ENDPOINT");
+
+OpenAIClient client = new OpenAIClient(new Uri(endpoint), new AzureKeyCredential(subscriptionKey));
+
+string prompt = "What is Azure OpenAI?";
+
+CompletionsOptions completionsOptions = new CompletionsOptions()
+{
+    Prompts = { prompt }
+};
+
+Response<Completions> completionsResponse = client.GetCompletions("myModelDeployment", completionsOptions);
+string completion = completionsResponse.Value.Choices[0].Text;
+Console.WriteLine($"Chatbot: {completion}");
